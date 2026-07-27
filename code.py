b6991f72cc685f2bdc28c72f32ef6eb4793ed6bf
aashish <aashishjuly15@gmail.com>
2026-07-27 18:35:22 +0530

    add mvp code

---
+++ code.py
@@ -0,0 +1,22 @@
+num1 = int(input("enter a number : " ))
+task = input("enter your task symble (eg- +,-,*,/) :")
+num2 = int(input("enter a number:"))
+if task == "+":
+    sum = num1 + num2 
+    print("here is your sum :" ,sum)
+elif task == "-":
+    diff = num1 - num2 
+    print("here is diffrence :" ,diff)
+elif task == "*":
+    multi = num1 * num2 
+    print("here is product:", multi)
+elif task == "/" :
+    if num2 == 0 :
+        print("bro you can't divide by zero")
+    else:    
+        divide = num1 / num2
+        print("here is your divide :" ,divide)
+else :
+    print ("task is not veild")
+
+

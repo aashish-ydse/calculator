80ac89c2394897b57caae485d226c71660f48497
aashish <aashishjuly15@gmail.com>
2026-08-03 09:53:52 +0530

    modify code - normal calculator

--- code.py
+++ code.py
@@ -1,43 +1,48 @@
-option = input("what are you want? (option - normal calculator , trigonomatric calculator,logarithm calculator,geometry calculator) :")
+option = input("what are you want? (option - normal calculator , trigonomatric calculator,logarithm calculator,geometry calculator):")
 if option == "normal calculator":
-    num1 = int(input("enter your first number :"))
-    task = input("enter your task symbol (eg- +,-,*,/,power(**),square root(sqrt)):")
-    if task=="sqrt" or task=="square root" or task=="square root(sqrt)":
-        square_root=num1**(.5)
-        print("square root:",square_root)
+    class Number :
+        def __init__(self, value):
+            self.value = value
+        def add(self, other):
+            return self.value + other.value
+        def subtract(self, other):
+            return self.value - other.value
+        def multiply(self, other):
+            return self.value * other.value
+        def divide(self, other):
+            if other.value == 0:
+                return print("Error: Division by zero")
+            elif other.value % self.value != 0:
+                result_type1 = input("What kind of answer do you want?(eg. floor division , modulus and division):")
+                if result_type1 == "floor division":
+                    print("Floor Division:", self.value // other.value)
+                elif result_type1 == "modulus":
+                    print("Modulus:", self.value % other.value)
+                elif result_type1 == "division":
+                    print("Division:", self.value / other.value)
+            else:
+                return self.value / other.value
+    s1 = Number(int(input("enter a number: ")))
+    symbol = input("Enter the operation you want to perform (+, -, *, /,**,sqrt): ")
+    if symbol == "sqrt":
+        print("Square root:", s1.value ** 0.5)
         exit()
-    else:    
-        num2=int(input("enter your second number:"))
-    if task=="+":
-        total=num1+num2 
-        print("total:",total)
-    elif task=="-":
-        difference=num1-num2 
-        print("difference:",difference)
-    elif task=="*":
-        product=num1*num2 
-        print("product:",product)
-    elif task=="/":
-        if num2==0:
-            print("bro you can't divide by zero")
-        elif (num1 % num2)!=0 :
-            result_type1=input("What kind of answer do you want?(eg. floor division , modulus and division):")
-            if result_type1=="floor division":
-                floor_division=num1//num2
-                print("floor division:",floor_division)
-            elif result_type1=="modulus":
-                modulus=num1%num2
-                print("modulus:",modulus)
-            elif result_type1=="division":
-                division=num1/num2
-                print("division:",division)
-        else:    
-            quotient=num1/num2
-            print("quotient:",quotient)
-    elif task=="**" or task=="power" or task=="power(**)":
-        power=num1**num2
-        print(num1,"to the power of",num2,"is",power)
+    s2 = Number(int(input("enter a number: ")))
+    if symbol == "+":
+        print("Addition:", s1.add(s2))
+    elif symbol == "-":
+        print("Subtraction:", s1.subtract(s2))
+    elif symbol == "*":
+        print("Multiplication:", s1.multiply(s2))
+    elif symbol == "/":
+        print(s1.divide(s2))
+    elif symbol == "**":
+        print("Exponentiation:", s1.value ** s2.value)
+    elif symbol == "sqrt":
+        print("Square root:", s1.value ** 0.5)
+
 elif option=="trigonomatric calculator":
+
     trig_id=str(input("enter your trigonometric identity(eg. sin,cos,ten,cot,sec,cosec):" ))
     angle=str(input("enter angle(eg. 0,30,45,60,90):"))
     if trig_id=="sin" and angle=="0" :
@@ -108,159 +113,159 @@ elif option=="logarithm calculator":
     num=int(input("enter num :"))
     y=math.log(num , base)
     print(y)
-elif option == "geometry calculator":
-    pi = 3.14159
-    dimension = input("enter your shape dimension (option - 2D and 3D) :")
-    if dimension == "2D":
-        calculation_type = input("what do you want to calculate (option - area or parimeter :")
-        if calculation_type == "area":
-            shape = input("enter your shape name (option - 1.Square ,2.Rectangle , 3.Circle, 4.Triangle, 5.Parallelogram, 6.Rhombus, 7.Kite, 8.Trapezium, 9.Ellipse, 10.Semicircle, 11.Sector, 12.Annulus) :" )
-            if shape == "square":
-                side = int(input("enter length of side :"))
-                area_square = side**2
+elif option=="geometry calculator":
+    pi=3.14159
+    dimension=input("enter your shape dimension (option - 2D and 3D) :")
+    if dimension=="2D":
+        calculation_type=input("what do you want to calculate (option - area or parimeter :")
+        if calculation_type=="area":
+            shape=input("enter your shape name (option - 1.Square ,2.Rectangle , 3.Circle, 4.Triangle, 5.Parallelogram, 6.Rhombus, 7.Kite, 8.Trapezium, 9.Ellipse, 10.Semicircle, 11.Sector, 12.Annulus) :" )
+            if shape=="square":
+                side=int(input("enter length of side :"))
+                area_square=side**2
                 print(area_square)
             elif shape=="rectangle":
-                side_len = int(input("enter length of rectangle :"))
-                side_wid = int(input("enter width of rectangle :"))
-                area_rectangle = side_len * side_wid
+                side_len=int(input("enter length of rectangle :"))
+                side_wid=int(input("enter width of rectangle :"))
+                area_rectangle=side_len*side_wid
                 print(area_rectangle)
-            elif shape == "circle":
-                cir_radius = int(input("enter radius of circle:"))
-                radius_square = cir_radius**2
-                area_circle = pi * radius_square
+            elif shape=="circle":
+                cir_radius=int(input("enter radius of circle:"))
+                radius_square=cir_radius**2
+                area_circle=pi*radius_square
                 print(area_circle)
-            elif shape == "triangle":
-                len_tri = int(input("enter length of triangle :"))
-                base_tri = int(input("enter base of triangle :"))
-                multi_len_base = len_tri * base_tri
-                area_triangle = multi_len_base/2
+            elif shape=="triangle":
+                len_tri=int(input("enter length of triangle :"))
+                base_tri=int(input("enter base of triangle :"))
+                multi_len_base=len_tri*base_tri
+                area_triangle=multi_len_base/2
                 print(area_triangle)
-            elif shape == "Parallelogram":
-                len_par = int(input("enter length of Parallelogram :"))
-                base_par = int(input("enter base of Parallelogram :"))
-                area_Parallelogram = len_par * base_par
+            elif shape=="Parallelogram":
+                len_par=int(input("enter length of Parallelogram :"))
+                base_par=int(input("enter base of Parallelogram :"))
+                area_Parallelogram=len_par*base_par
                 print(area_Parallelogram)
-            elif shape == "Rhombus":
-                diagonal1 = int(input("enter 1st diagonal :"))
-                diagonal2 = int(input("enter 2nd diagonal :"))
-                multi_d1d2 = diagonal1 * diagonal2
-                area_Rhombus = multi_d1d2/2
+            elif shape=="Rhombus":
+                diagonal1=int(input("enter 1st diagonal :"))
+                diagonal2=int(input("enter 2nd diagonal :"))
+                multi_d1d2=diagonal1*diagonal2
+                area_Rhombus=multi_d1d2/2
                 print(area_Rhombus)
-            elif shape == "Kite":
-                diagonal1 = int(input("enter 1st diagonal :"))
-                diagonal2 = int(input("enter 2nd diagonal :"))
-                multi_d1d2 = diagonal1 * diagonal2
-                area_kite = multi_d1d2/2
+            elif shape=="Kite":
+                diagonal1=int(input("enter 1st diagonal :"))
+                diagonal2=int(input("enter 2nd diagonal :"))
+                multi_d1d2=diagonal1 * diagonal2
+                area_kite=multi_d1d2/2
                 print(area_kite)
-            elif shape == "Trapezium":
-                side = int(input("enter side of Trapezium :"))
-                base_tra = int(input("enter base of Trapezium :"))
-                hight_tra = int(input("enter hight of Trapezium :"))
-                sum_side_base = side + base_tra
-                half_hight = hight_tra/2
-                area_Trapezium = sum_side_base * half_hight 
+            elif shape=="Trapezium":
+                side=int(input("enter side of Trapezium :"))
+                base_tra=int(input("enter base of Trapezium :"))
+                hight_tra=int(input("enter hight of Trapezium :"))
+                sum_side_base=side + base_tra
+                half_hight=hight_tra/2
+                area_Trapezium=sum_side_base*half_hight 
                 print(area_Trapezium)
-            elif shape == "Semicircle":
-                cir_radius = int(input("enter radius of circle:"))
-                radius_square = cir_radius**2
-                area_semicircle = (pi * radius_square)/2
+            elif shape=="Semicircle":
+                cir_radius=int(input("enter radius of circle:"))
+                radius_square=cir_radius**2
+                area_semicircle=(pi * radius_square)/2
                 print(area_semicircle)
-            elif shape == "Sector":
-                cir_radius = int(input("enter radius of circle:"))
-                angle = int(input("enter angle of sector :"))
-                radius_square = cir_radius**2
-                multi_pai_rad = (pi * radius_square)
-                angle_div = angle/360
-                area_sector = multi_pai_rad * angle_div
+            elif shape=="Sector":
+                cir_radius=int(input("enter radius of circle:"))
+                angle=int(input("enter angle of sector :"))
+                radius_square=cir_radius**2
+                multi_pai_rad=(pi * radius_square)
+                angle_div=angle/360
+                area_sector=multi_pai_rad * angle_div
                 print(area_sector)
-            elif shape == "annulus":
-                r1 = int(input("enter inner radius :"))
-                r2 = int(input("enter outer radius :"))
-                r1_square = r1**2
-                r2_square = r2**2
-                r2_r1 = r2_square - r1_square
-                area_annulus = pi * r2_r1
+            elif shape=="annulus":
+                r1=int(input("enter inner radius :"))
+                r2=int(input("enter outer radius :"))
+                r1_square=r1**2
+                r2_square=r2**2
+                r2_r1=r2_square-r1_square
+                area_annulus=pi*r2_r1
                 print(area_annulus)
-            elif shape == "polygon":
-                side = int(input("enter numbers of sides of polygon :"))
-                length = int(input("enter length of side :"))
+            elif shape=="polygon":
+                side=int(input("enter numbers of sides of polygon :"))
+                length=int(input("enter length of side :"))
                 import math 
-                tan = math.tan(math.pi/side)
-                area_polygon = (side*(length**2))/(4*tan)
+                tan=math.tan(math.pi/side)
+                area_polygon=(side*(length**2))/(4*tan)
                 print(area_polygon)
             else:
                 print("sorry , i'm not capable for this calculation!")
-        elif calculation_type == "perimeter":
-            shape = input("enter your shape name (option - 1.Square ,2.Rectangle , 3.Circle, 4.Triangle, 5.Parallelogram, 6.Rhombus, 7.Kite, 8.Trapezium, 9.Ellipse, 10.Semicircle, 11.Sector, 12.Annulus :" )
-            if shape == "square":
-                side = int(input("enter length of side :"))
-                perimeter_square = side*4
+        elif calculation_type=="perimeter":
+            shape=input("enter your shape name (option - 1.Square ,2.Rectangle , 3.Circle, 4.Triangle, 5.Parallelogram, 6.Rhombus, 7.Kite, 8.Trapezium, 9.Ellipse, 10.Semicircle, 11.Sector, 12.Annulus :" )
+            if shape=="square":
+                side=int(input("enter length of side :"))
+                perimeter_square=side*4
                 print(perimeter_square)
             elif shape=="rectangle":
-                side_len = int(input("enter length of rectangle :"))
-                side_wid = int(input("enter width of rectangle :"))
-                perimeter_rectangle = (side_len + side_wid) * 2
+                side_len=int(input("enter length of rectangle :"))
+                side_wid=int(input("enter width of rectangle :"))
+                perimeter_rectangle=(side_len + side_wid)*2
                 print(perimeter_rectangle)
-            elif shape == "circle":
-                cir_radius = int(input("enter radius of circle:"))
-                pai = 3.1415
-                radius_2 = cir_radius*2
-                perimeter_circle = pai * radius_2
+            elif shape=="circle":
+                cir_radius=int(input("enter radius of circle:"))
+                pai=3.1415
+                radius_2=cir_radius*2
+                perimeter_circle=pai*radius_2
                 print(perimeter_circle)
-            elif shape == "triangle":
-                len_tri_side1 = int(input("enter length of side 1 :"))
-                len_tri_side2 = int(input("enter length of side 2 :"))
-                base_tri = int(input("enter base of triangle :"))
-                perimeter_triangle = len_tri_side1 + len_tri_side2 + base_tri
+            elif shape=="triangle":
+                len_tri_side1=int(input("enter length of side 1 :"))
+                len_tri_side2=int(input("enter length of side 2 :"))
+                base_tri=int(input("enter base of triangle :"))
+                perimeter_triangle=len_tri_side1+len_tri_side2+base_tri
                 print(perimeter_triangle)
-            elif shape == "Parallelogram":
-                len_par = int(input("enter length of side :"))
-                base_par = int(input("enter base of Parallelogram :"))
-                perimeter_Parallelogram = (len_par + base_par) * 2
+            elif shape=="Parallelogram":
+                len_par=int(input("enter length of side :"))
+                base_par=int(input("enter base of Parallelogram :"))
+                perimeter_Parallelogram=(len_par + base_par)*2
                 print(perimeter_Parallelogram)
-            elif shape == "Rhombus":
-                side = int(input("enter length of side :"))
-                perimeter_Rhombus = side * 4
+            elif shape=="Rhombus":
+                side=int(input("enter length of side :"))
+                perimeter_Rhombus=side*4
                 print(perimeter_Rhombus)
-            elif shape == "Kite":
-                side1 = int(input("enter length of side 1st :"))
-                side2 = int(input("enter length of side 2nd :"))
-                perimeter_kite = (side1 + side2)*2
+            elif shape=="Kite":
+                side1=int(input("enter length of side 1st :"))
+                side2=int(input("enter length of side 2nd :"))
+                perimeter_kite=(side1 + side2)*2
                 print(perimeter_kite)
-            elif shape == "Trapezium":
-                side1 = int(input("enter length of side 1st :"))
-                side2 = int(input("enter length of side 2nd :"))
-                side3 = int(input("enter length of side 3ed :"))
-                side4 = int(input("enter length of side 4th :"))
-                perimeter_Trapezium = side1 + side2 + side3 + side4
+            elif shape=="Trapezium":
+                side1=int(input("enter length of side 1st :"))
+                side2=int(input("enter length of side 2nd :"))
+                side3=int(input("enter length of side 3ed :"))
+                side4=int(input("enter length of side 4th :"))
+                perimeter_Trapezium=side1+side2+side3+side4
                 print(perimeter_Trapezium)
-            elif shape == "Semicircle":
-                cir_radius = int(input("enter radius of circle:"))
-                pai = 3.1415
-                radius_2 = cir_radius*2
-                perimeter_semicircle = (pai*cir_radius) + radius_2
+            elif shape=="Semicircle":
+                cir_radius=int(input("enter radius of circle:"))
+                pai=3.1415
+                radius_2=cir_radius*2
+                perimeter_semicircle=(pai*cir_radius)+radius_2
                 print(perimeter_semicircle)
-            elif shape == "Sector":
-                cir_radius = int(input("enter radius of circle:"))
+            elif shape=="Sector":
+                cir_radius=int(input("enter radius of circle:"))
                 angle = int(input("enter angle of sector :"))
                 pai = 3.1415
                 radius_2 = cir_radius*2
-                multi_pai_rad = (pai * radius_2)
-                angle_div = angle/360
-                perimeter_sector = (multi_pai_rad * angle_div) + radius_2
+                multi_pai_rad=(pai*radius_2)
+                angle_div=angle/360
+                perimeter_sector=(multi_pai_rad * angle_div)+radius_2
                 print(perimeter_sector)
-            elif shape == "annulus":
-                r1 = int(input("enter inner radius :"))
-                r2 = int(input("enter outer radius :"))
-                r2_r1 = r2 + r1 
-                pai = 3.1415
-                pai2 = pai*2
-                perimeter_annulus = pai2 * r2_r1
+            elif shape=="annulus":
+                r1=(input("enter inner radius :"))
+                r2=int(input("enter outer radius :"))
+                r2_r1=r2 + r1 
+                pai=3.1415
+                pai2=pai*2
+                perimeter_annulus=pai2*r2_r1
                 print(perimeter_annulus)
-            elif shape == "polygon":
-                side = int(input("enter numbers of sides of polygon :"))
-                length = int(input("enter length of side :"))
-                perimeter_polygon = side * length
+            elif shape=="polygon":
+                side=int(input("enter numbers of sides of polygon :"))
+                length=int(input("enter length of side :"))
+                perimeter_polygon=side*length
                 print(perimeter_polygon)
             else:
                 print("sorry , i'm not capable for this calculation!")
